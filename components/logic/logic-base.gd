bda5e040428aad8823ceebedaf1a579f6cadd6d2
OliPohl <oliver@pohl66.de>
2025-03-14 22:09:55 +0100

    create logic icons and base files

---
+++ components/logic/logic-base.gd
@@ -0,0 +1,4 @@
+@tool
+@icon("res://components/logic/icons/logic-blue.png")
+class_name LogicBase extends Logic
+## This is the base class for all logic nodes. It provides a common interface for all logic nodes to implement.
---
+++ components/logic/logic.gd
@@ -0,0 +1,4 @@
+@tool
+@icon("res://components/logic/icons/logic-gray.png")
+class_name Logic extends Node
+## Abstract Logic class for creating custom logic nodes.
