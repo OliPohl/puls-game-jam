c0247d5f31f2e6ff31cef47e2056b2e67e0a069c
Robert Moegenburg <r.moegenburg@t-online.de>
2025-03-15 22:19:41 +0100

    moving_plattform_fix

--- scenes/objects/moving_plattform/moving_plattform.gd
+++ scenes/objects/moving_plattform/moving_plattform.gd
@@ -2,6 +2,18 @@ extends StaticBody2D
 @onready var _collision_node : CollisionShape2D =$Moving_plattform/CollisionShape2D
 @onready var _visuals : Node2D = $Moving_plattform
 @onready var _animation_player : AnimationPlayer = $AnimationPlayer
+@onready var _player_detect_zone : Area2D = $Moving_plattform/Player_detect_zone
+
+func _ready() -> void:
+    _player_detect_zone.body_entered.connect(_on_body_entered)
+    _player_detect_zone.body_exited.connect(_on_body_exited)
+
+func _on_body_entered() ->void:
+    _animation_player.play("move")
+func _on_body_exited() ->void:
+    _animation_player.stop()
+
+
 var collision: bool:
     set(value):
         if (collision != value):
